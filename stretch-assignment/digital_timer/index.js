b4ee359efe23f6866847de8c472d0d5bc304b3c4
Will Schulz <will.schulz29@gmail.com>
2019-01-28 18:04:35 -0600

    so close to stretch

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,175 @@
+// msTens
+
+for (let i=0; i<=10; i++){
+    const milliTens = document.querySelector('msTens');
+    setInterval(function(){ 
+        const x = 1;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 2;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 3;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 4;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 5;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 6;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 7;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 8;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 9;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    setInterval(function(){ 
+        const x = 0;
+        const y = x.toString();
+        document.getElementById("msTens").innerHTML = y;
+    }, 10);
+    // msHundreds
+    const milliHundreds = document.querySelector('msHundreds');
+    setInterval(function(){ 
+        const x = 1;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 2;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 3;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 4;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 5;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 6;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 7;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 8;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 9;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    setInterval(function(){ 
+        const x = 0;
+        const y = x.toString();
+        document.getElementById("msHundreds").innerHTML = y;
+    }, 100);
+    
+    // Seconds
+    const firstSeconds = document.querySelector('secondOnes');
+    setInterval(function(){ 
+        const x = 1;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 2;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 3;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 4;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 5;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 6;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 7;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 8;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 9;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    setInterval(function(){ 
+        const x = 0;
+        const y = x.toString();
+        document.getElementById("secondOnes").innerHTML = y;
+    }, 1000);
+    // Ten Seconds
+    const tensSeconds = document.querySelector('secondTens');
+    setInterval(function(){ 
+    const x = 1;
+    const y = x.toString();
+    document.getElementById("secondTens").innerHTML = y;
+}, 10000);
+}
+
+
+
+
+
+
+
+
